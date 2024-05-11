c3d1c55c8fe2f1eb8840486198436e4856e451fd
Ansh Mahajan <anshmahajan05102002@gmail.com>
2024-05-11 18:06:21 +0530

    set worker and thread as 1

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,10 +1,10 @@
 import os
 
-workers = int(os.environ.get('GUNICORN_PROCESSES', '2'))
+workers = int(os.environ.get('GUNICORN_PROCESSES', '1'))
 
-threads = int(os.environ.get('GUNICORN_THREADS', '4'))
+threads = int(os.environ.get('GUNICORN_THREADS', '1'))
 
-# timeout = int(os.environ.get('GUNICORN_TIMEOUT', '120'))
+timeout = int(os.environ.get('GUNICORN_TIMEOUT', '120'))
 
 bind = os.environ.get('GUNICORN_BIND', '0.0.0.0:8080')
 
